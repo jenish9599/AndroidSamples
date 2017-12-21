eb1d1fe0aeea7c3ce8c36554381e600c3984d404
jenish9599 <pateljenish813@gmail.com>
2017-12-21 22:00:05 +0530

    GifPlay added

---
+++ Animations/GifPlay/app/src/main/java/com/jenish9599/android/gifplay/MainActivity.java
@@ -0,0 +1,19 @@
+package com.jenish9599.android.gifplay;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageView imageView = (ImageView)findViewById(R.id.imageView);
+        Glide.with(this).load(R.raw.gifff).asGif().into(imageView);
+    }
+}
